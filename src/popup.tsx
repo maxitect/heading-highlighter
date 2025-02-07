bc84d1402f30de9406bf415ba1ace34aa13c03dd
maxitect <maxime.downe@gmail.com>
2025-02-07 22:32:56 +0000

    feat: make popup.tsx to render toggle button and handle state through plasmo messaging service

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,11 +1,51 @@
-import { CountButton } from "~features/count-button"
-
 import "~style.css"
 
+import { useState } from "react"
+
+import Toggle from "~features/toggle"
+
 function IndexPopup() {
+  const [isEnabled, setIsEnabled] = useState<boolean>(false)
+
+  const handleEnabledClick = async () => {
+    const newState = !isEnabled
+    console.log("Toggle clicked, new state:", newState)
+    setIsEnabled(newState)
+
+    const tabs = await chrome.tabs.query({})
+    tabs.forEach((tab) => {
+      if (tab.id) {
+        try {
+          chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            func: (state) => {
+              window.postMessage(
+                {
+                  type: "TOGGLE_HIGHLIGHTS",
+                  isEnabled: state
+                },
+                "*"
+              )
+            },
+            args: [newState]
+          })
+          console.log("Sent new state to tab:", tab.id)
+        } catch (error) {
+          console.error("Error sending to tab:", tab.id, error)
+        }
+      }
+    })
+  }
+
   return (
-    <div className="plasmo-flex plasmo-items-center plasmo-justify-center plasmo-h-16 plasmo-w-40">
-      <CountButton />
+    <div className="plasmo-flex plasmo-items-center plasmo-justify-center plasmo-h-64 plasmo-w-56 plasmo-flex-col plasmo-bg-slate-200">
+      <h1 className="plasmo-text-lg plasmo-mb-10 plasmo-text-center">
+        Heading
+        <br />
+        Highlighter
+      </h1>
+      <Toggle checked={isEnabled} onChange={handleEnabledClick} />
+      <h2>{isEnabled ? "ON" : "OFF"}</h2>
     </div>
   )
 }
