ad463c8375eef3e5ec85b4637c6e18150d7186f3
maxitect <maxime.downe@gmail.com>
2025-02-11 16:46:31 +0000

    refactor: delete unused util getCurrentTabId.ts

--- src/utils/getCurrentTabId.ts
+++
@@ -1,5 +0,0 @@
-export default async function getCurrentTabId() {
-  let queryOptions = { active: true, lastFocusedWindow: true }
-  let [tab] = await chrome.tabs.query(queryOptions)
-  return tab.id
-}
